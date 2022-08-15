36b1daa63e4fc832318ce4acff919864bd7e5ce8
ayushsingh0607 <ayushraj0607@gmail.com>
2022-08-15 11:12:41 +0530

    done

--- src/constants/Navbar/Navbar.jsx
+++ src/constants/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='app__navbar'>
         <div className='app__navbar-head'>
-            <h1 className='logo'> Relax</h1>
+            <h1 className='logo'> Relax.com</h1>
             <div className='app__navbar-button'>
                 <button type="button" className='custom_button'>Register</button>
                 <button type="button" className='custom_button'>Login</button>
